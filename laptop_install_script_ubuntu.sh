82748defe15103ba12343e91fff72d5e7320a3e5
Stomp Lee <stomp@lee.com>
2017-10-30 07:41:26 -0700

    added laptop install script with nvidia fix

---
+++ laptop_install_script_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update && apt-get upgrade -y
+apt-get install wget curl terminator vim git youtube-dl shutter keepassx vim vlc audacious audacity python python-pip -y
+
+# sublime 3 stuff
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get install sublime-text -y
+
+# aws cli
+pip install --upgrade pip
+pip install --upgrade awscli
+
+# fix nvidia
+apt-get purge nvidia* -y
+apt-get install nvidia-331 -y
