7c9bc17866a895f8b11af62baee8092598ffeccb
Stomp Lee <stomp@lee.com>
2017-10-30 11:46:25 -0700

    added jq

--- laptop_install_script_ubuntu.sh
+++ laptop_install_script_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get upgrade -y
-apt-get install wget curl terminator vim git youtube-dl shutter keepassx vim vlc audacious audacity python python-pip -y
+apt-get install jq wget curl terminator vim git youtube-dl shutter keepassx vim vlc audacious audacity python python-pip -y
 
 # sublime 3 stuff
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
